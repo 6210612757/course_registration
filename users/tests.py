7f62f926fca09a1afe9c40ddcddcf11ca0622d70
Wanchana Moonmuang <6210612757@cn330>
2021-10-01 16:07:23 +0700

    Done tests.py & add check std_id in register

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,137 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from course.models import *
 
 # Create your tests here.
+class Login_out(TestCase):
+
+    def setUp(self):
+        
+        user = User.objects.create(username = "user1", password = make_password("1234"),email="user1@example.com")
+
+    def test_index_not_authenticated(self):
+        c = Client()
+        
+        response = c.get(reverse('users:index'))
+        self.assertEqual(response.status_code, 302) #Using Redirect code 302
+
+    def test_index_authenticated(self):
+        user = User.objects.get(username = "user1")
+        c = Client()
+        c.force_login(user)
+        response = c.get(reverse('users:index'))
+        self.assertEqual(response.status_code, 200) #Using Render code 200
+        self.assertTemplateUsed(response, "users/index.html") #Load HTML
+
+    def test_login_authenticated(self):
+        user = User.objects.get(username = "user1")
+        c = Client()
+        c.force_login(user)
+        response = c.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 302) 
+
+    def test_login_success(self) :
+        c = Client()
+        response = c.post(reverse('users:login'),{'username' : 'user1','password':'1234'})
+        self.assertEqual(response.status_code, 302) 
+
+    def test_login_not_success(self) :
+        c = Client()
+        response = c.post(reverse('users:login'),{'username' : 'user1','password': ''})
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/login.html")
+    
+    def test_direct_login(self) :
+        c = Client()
+        response = c.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/login.html")
+
+    def test_logout(self) :
+        c = Client()
+        response = c.get(reverse('users:logout'))
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/login.html")
+
+class Register(TestCase):
+    def setUp(self):
+        
+        user = User.objects.create(username = "user1", password = make_password("1234"),email="user1@example.com")
+        student = Student(user = user,std_id = '6200000000')
+        student.save()
+
+    def test_index_register(self) :
+        c = Client()
+        response = c.get(reverse('users:register'))
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/register.html")
+
+    def test_register_success(self) :
+        c = Client()
+        response = c.post(reverse('users:register'),{'username' : 'newuser',
+                                                'password': '1234',
+                                                're_password': '1234',
+                                                'email': 'newuser@student.com',
+                                                'first_name' : 'sawadee',
+                                                'last_name' : 'tansamachick',
+                                                'std_id' : '6200000001',
+                                                })
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/login.html")
+
+    def test_register_duplicate_username(self):
+        c = Client()
+        response = c.post(reverse('users:register'),{'username' : 'user1',
+                                                'password': '1234',
+                                                're_password': '1234',
+                                                'email': 'newuser@student.com',
+                                                'first_name' : 'sawadee',
+                                                'last_name' : 'tansamachick',
+                                                'std_id' : '6200000001',
+                                                })
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/register.html")
+
+    def test_register_duplicate_std_id(self):
+        c = Client()
+        response = c.post(reverse('users:register'),{'username' : 'newuser',
+                                                'password': '1234',
+                                                're_password': '1234',
+                                                'email': 'newuser@student.com',
+                                                'first_name' : 'sawadee',
+                                                'last_name' : 'tansamachick',
+                                                'std_id' : '6200000000',
+                                                })
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/register.html") 
+
+    def test_register_check_valid_std_id(self):
+        c = Client()
+        response = c.post(reverse('users:register'),{'username' : 'newuser',
+                                                'password': '1234',
+                                                're_password': '1234',
+                                                'email': 'newuser@student.com',
+                                                'first_name' : 'sawadee',
+                                                'last_name' : 'tansamachick',
+                                                'std_id' : '6200000000111111',
+                                                })
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/register.html")
+
+    def test_register_check_repassword(self):
+        c = Client()
+        response = c.post(reverse('users:register'),{'username' : 'newuser',
+                                                'password': '12345',
+                                                're_password': 'something',
+                                                'email': 'newuser@student.com',
+                                                'first_name' : 'sawadee',
+                                                'last_name' : 'tansamachick',
+                                                'std_id' : '6200000001',
+                                                })
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, "users/register.html")
+
+    
--- users/views.py
+++ users/views.py
@@ -51,6 +51,8 @@ def register_view(request):
         std_id = request.POST["std_id"]
         if User.objects.filter(username = username).first():
             return render(request, "users/register.html", {"fail": "This username is already taken"})
+        if Student.objects.filter(std_id = std_id).first():
+            return render(request, "users/register.html", {"fail": "Student ID already taken"})
         if (not std_id.isnumeric()) or (len(std_id) > 10 or len(std_id) < 10):
             return render(request, "users/register.html", {"fail": "Student ID must be an integer with length of 10"})
         if password != re_password:
